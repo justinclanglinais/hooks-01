bcef7093ec5700accb4f5581c5fe4b482f8e7f50
Justin Langlinais <justinclanglinais@gmail.com>
2020-09-26 14:37:28 -0700

    feat(ExampleWithManyStates): add more

--- src/components/ExampleWithManyStates.js
+++ src/components/ExampleWithManyStates.js
@@ -4,15 +4,28 @@ function ExampleWithManyStates () {
     const [ age, setAge ] = useState(37);
     const [ fruit, setFruit ] = useState('banana');
     const [ todos, setTodos ] = useState([{ text: 'Learn Hooks'}]);
+    
+    const handleChangeFruit = (e) => {
+    setFruit(e.target.value)
+}
+
     return (
         <div>
-            <button onClick={ () => setAge(age + 1) }>
-                Increase my age!
-            </button>
-            <p>You are {age} years old</p>
-            <button onClick={ () => setAge(age - 1) }>
-                Decrease my age!
-            </button>
+            <div>
+                <button onClick={ () => setAge(age + 1) }>
+                    Increase my age!
+                </button>
+                <p>You are {age} years old</p>
+                <button onClick={ () => setAge(age - 1) }>
+                    Decrease my age!
+                </button>
+            </div>
+            <div>
+                <p>{fruit}</p>
+                <form>
+                    <input onChange={handleChangeFruit} value={fruit}></input>
+                </form>
+            </div>
         </div>
     );
 };
